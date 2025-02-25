18b115d2b000db9e06837c2ba0f0960d86564c0f
isaac0969 <issyeabrham@gmail.com>
2025-02-25 21:05:58 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';  // You can adjust the path if needed
+import { Provider } from 'react-redux';
+import store from './store.js'; // Make sure the path to store.js is correct
+
+// Render the app with the Provider to make Redux available throughout the app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
